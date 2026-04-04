78f4d99eb030cea421d0e5d41066bdbcf782f88b
Rudra Pratap Singh <98646005+Rudra-Stark@users.noreply.github.com>
2026-04-04 16:40:14 +0530

    Implement performance test for lists vs NumPy arrays
    Added performance comparison between Python lists and NumPy arrays.

--- Beginner/Numpy/numpy_time.py
+++ Beginner/Numpy/numpy_time.py
@@ -1,1 +1,18 @@
+import numpy as np 
+from time import perf_counter 
 
+# This checks how long it takes to process a list without NumPy
+
+python_list = [i for i in range(10000)]
+start_time = perf_counter()
+python_list = [i+5 for i in python_list]
+end_time = perf_counter()
+print(f"Python list : {end_time-start_time:.10f}")
+
+# This is the list created by Numpy 
+
+np_array = np.array([i for i in range(10000)])
+start_time1 = perf_counter()
+np_array+=1
+end_time1 = perf_counter()
+print(f"Numpy list  : {end_time1 - start_time1:.10f}")
